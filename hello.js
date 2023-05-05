335c42b716b5eaa660f9597b8f5dfec94a78e2bb
Toraskarpg <pgtoraskar@gmail.com>
2023-05-05 08:52:58 +0530

    new js file
    new js file

---
+++ hello.js
@@ -0,0 +1,18 @@
+
+
+
+
+console.log("Pratiksha Toraskar..");
+var firstName = "Pratiksha"; 
+console.log(firstName);
+var age;  //variable Declaration
+age="22"; //Variable initialization
+console.log(age);
+
+var holder;//Variable Declaration
+console.log(holder); // undefined
+holder = "My Spec"; // Variable Initialization
+console.log(holder);
+
+holder = "Sachin Spec"; // Variable update
+console.log(holder); //
