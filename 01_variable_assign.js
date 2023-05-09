ded8140d2c609c94b1a095cbfc6c677d3a54f998
Toraskarpg <pgtoraskar@gmail.com>
2023-05-09 18:10:35 +0530

    variable
    variable

---
+++ 01_variable_assign.js
@@ -0,0 +1,12 @@
+var myFullName;                          //Declare variable
+console.log(myFullName);                 
+ myFullName="Pratiksha Toraskar";        //Assign value to variable
+console.log(myFullName);              
+myFullName = "Pratiksha Ganesh Toraskar"; //updatevariable value
+console.log(myFullName);
+var myLove;
+//console.log(myLove);
+myLove="I Love Only JavaScript";
+console.log(myLove);
+var firstName="Pratiksha", lastName="Toraskar",age="25", address="Pune, Maharashtra", pincode="411028", gender="Female",isMarried="Yes";
+console.log(firstName, lastName, age, address, pincode, gender, isMarried); 
