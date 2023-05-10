b24b291b6d286744a33b38555185d99edd854741
Toraskarpg <pgtoraskar@gmail.com>
2023-05-10 18:11:26 +0530

    declaration of variable and data type
    Declare a variable and print there data type.
    and diff between Null and Undefined

---
+++ 02_variableBasicsAssign.js
@@ -0,0 +1,45 @@
+// declaration of String datatype
+var first = "*************************String Data Type**********************"
+var myName= "Pratiksha Sachin Kamthe";
+var clgName = "Vaadia";
+var stream = "Bsc Computer Science";
+console.log(first);
+console.log("My Name is:", myName,"Type:", typeof myName);
+console.log("College Name is:", clgName, "Type:", typeof clgName);
+console.log("My Stream is:", stream, "Type:", typeof stream);
+
+// declaration of Number datatype
+
+var second="***********************Number Data Type**********************"
+var birthDate= 17;
+var clgCode = 93;
+var rollNo = 200017;
+console.log(second);
+console.log("My Birth Date is:" , birthDate, "Type:", typeof birthDate);
+console.log("My college Code is: ", clgCode , "Type:", typeof clgCode);
+console.log("My Roll No is: ", rollNo, "Type:", typeof rollNo);
+
+// declaration of boolean datatype
+
+var third= "********************Boolean**********************"
+var isBachelor= true;
+var isGovernment= false;
+var fromScience= true;
+console.log(third);
+console.log("Sudent is Bachelor:", isBachelor, "Type:" , typeof isBachelor);
+console.log("college is Government or Private: ", isGovernment, "Type:", typeof isGovernment);
+console.log("Student is From Science Faculty:",fromScience , "Type:", typeof fromScience );
+
+// diffrence between null And undefined
+var diff ="********************diffrence between Null and undefined*************************"
+var diffNull="1.Data type of null is object, 2.null means value is null to varibale."
+var diffUndefined="1.data type of undefined is undefined 2. undefined means variable is declared but value is not assigned yet."
+console.log(diff);
+console.log(diffNull);
+console.log(diffUndefined);
+
+
+
+
+
+
