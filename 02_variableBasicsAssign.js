eaf9b41cbc6db6b67555e5838ea0ef1b9c626741
Toraskarpg <pgtoraskar@gmail.com>
2023-05-10 18:57:13 +0530

    Update
    Update

--- 02_variableBasicsAssign.js
+++ 02_variableBasicsAssign.js
@@ -3,10 +3,12 @@ var first = "*************************String Data Type**********************"
 var myName= "Pratiksha Sachin Kamthe";
 var clgName = "Vaadia";
 var stream = "Bsc Computer Science";
+var star="";
 console.log(first);
 console.log("My Name is:", myName,"Type:", typeof myName);
 console.log("College Name is:", clgName, "Type:", typeof clgName);
 console.log("My Stream is:", stream, "Type:", typeof stream);
+console.log(star);
 
 // declaration of Number datatype
 
@@ -18,6 +20,7 @@ console.log(second);
 console.log("My Birth Date is:" , birthDate, "Type:", typeof birthDate);
 console.log("My college Code is: ", clgCode , "Type:", typeof clgCode);
 console.log("My Roll No is: ", rollNo, "Type:", typeof rollNo);
+console.log(star);
 
 // declaration of boolean datatype
 
@@ -29,6 +32,7 @@ console.log(third);
 console.log("Sudent is Bachelor:", isBachelor, "Type:" , typeof isBachelor);
 console.log("college is Government or Private: ", isGovernment, "Type:", typeof isGovernment);
 console.log("Student is From Science Faculty:",fromScience , "Type:", typeof fromScience );
+console.log(star);
 
 // diffrence between null And undefined
 var diff ="********************diffrence between Null and undefined*************************"
@@ -37,6 +41,7 @@ var diffUndefined="1.data type of undefined is undefined 2. undefined means vari
 console.log(diff);
 console.log(diffNull);
 console.log(diffUndefined);
+console.log(star);
 
 
 
