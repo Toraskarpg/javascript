070c209ed665313af7ce998838bcc7014e15b602
Toraskarpg <pgtoraskar@gmail.com>
2023-05-10 19:01:10 +0530

    Declare & Initialization use of typeof operator
    Declare and Initialization of variables and log there data type along with there values by  using typeof operator.

---
+++ 03_typeOf.js
@@ -0,0 +1,18 @@
+var bankName;
+console.log(bankName);
+console.log("Bank name data type before Initialization:", typeof bankName);
+var bankName="SBI Bank";
+var marks= '90%';
+console.log("Bank Name-->", bankName);
+console.log("Bank Name data type after Initialization: ", typeof bankName,"Marks-->", marks, "Data Type-->", typeof marks);
+var star="";
+console.log(star);
+var isWorking;
+console.log("variable before Initialization:", isWorking);
+var isWorking=true;
+console.log("variable after Initialization:", isWorking, "type-->", typeof isWorking);
+console.log(star);
+var totalCount="Hundred and Seven";
+console.log(totalCount, "Type of totalCount-->",typeof totalCount);
+var totalCount= 107;
+console.log(totalCount, "type of totalCount-->", typeof totalCount);
