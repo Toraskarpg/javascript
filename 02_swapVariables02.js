58b6874375ae389fb4450c33e835b4dbb18cf86c
Toraskarpg <pgtoraskar@gmail.com>
2023-05-11 19:35:56 +0530

    assignment foe swapping varibale
    assignment foe swapping varibale

---
+++ 02_swapVariables02.js
@@ -0,0 +1,26 @@
+var sweety = "Sweety";
+var cutie = "Cutie";
+var star ="";
+console.log("Values before swapping is--> ", sweety, cutie);
+console.log(star);
+var num1= sweety;
+sweety= cutie;
+cutie= num1;
+console.log("Values after swapping is--> ", sweety, cutie);
+console.log(star);
+
+var num1=100;
+var num2=200;
+var num3=300;
+console.log("Values before swapping--> ", num1, num2, num3);
+console.log(star);
+var num4=num1;
+num1= num2;
+num2= num3;
+num3= num4;
+console.log("values after swapping--> ", num1, num2, num3);
+console.log(star);
+
+
+
+
