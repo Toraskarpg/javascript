8178b769cec36dfb559c44edf2dcc5a4590148db
Toraskarpg <pgtoraskar@gmail.com>
2023-05-12 18:54:55 +0530

    calculate length of string
    calculate length of string

---
+++ 04_stringAssig01.js
@@ -0,0 +1,16 @@
+function stringBasics(){
+    console.log("My Dream Company is Microsoft");   
+}
+stringBasics();
+console.log("----------------------------");
+
+var hobby1 = "Reading";
+var hobby2 = "Travelling";
+var hobby3 = "Coding";
+console.log("My Hobbies are--> ", hobby1, hobby2, hobby3);
+var hobby1Length= hobby1.length;
+var hobby2Length= hobby2.length;
+var hobby3Length= hobby3.length;
+console.log("Total No of charachter is--> ", hobby1Length+hobby2Length+hobby3Length);
+
+
