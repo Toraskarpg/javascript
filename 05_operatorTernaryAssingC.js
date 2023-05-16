a38b0f7faf6d5b328e130a77c9fee5c4762696ef
Toraskarpg <pgtoraskar@gmail.com>
2023-05-17 00:52:15 +0530

    Function with turnary operator
    Function with argument using turnary operator

---
+++ 05_operatorTernaryAssingC.js
@@ -0,0 +1,22 @@
+console.log(`Marriage Eligibility`);
+
+function maleMarriageEligibility(gender, age, boyName) {
+    var result = (gender=="Male" && age>=21)? `Hey ${boyName} you are eligible for marriage.` : `${boyName} you are not eligible for marriage.`;
+    return result;
+}
+var result = maleMarriageEligibility("Male", 25, "Billgates");
+console.log(result);
+var result = maleMarriageEligibility("Male", 17, "Stew Jobs");
+console.log(result);
+
+console.log(`-------------------------------------------------------`);
+
+console.log();
+function maleMarriageEligibility(gender, age, girlName){
+var result = (gender== "Female" && age>=18)? `Hey ${girlName} you are eligible for marraige.` : `${girlName} you are not eligibale for marriage.`;
+return result;
+}
+var result = maleMarriageEligibility("Female", 16, "Jenifer");
+console.log(result);
+var result = maleMarriageEligibility("Female", 27, "Malinda Gates");
+console.log(result);
