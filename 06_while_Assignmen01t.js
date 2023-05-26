7873eb0b698165760def1123682e1459c6980477
Toraskarpg <pgtoraskar@gmail.com>
2023-05-26 11:49:08 +0530

    while loop
    using while loop print even and odd numbers, table of 5 and 10

---
+++ 06_while_Assignmen01t.js
@@ -0,0 +1,77 @@
+console.log(`-----------------While loop assingment----------------`);
+console.log(`1] WAP to print numbers from 5 to 15 by incrementing 1 `);
+let i=5;
+while(i<=15)
+{
+    console.log(i);
+    i= i+1;
+}
+console.log(`-----------------------------------------------------------`);
+
+console.log(`WAP to print numbers from 50 to 40  decrementing by 1`);
+let j = 50;
+while(j>=40)
+{
+    console.log(j);
+    j= j-1;
+}
+
+console.log(`-----------------------------------------------------------`);
+console.log(`WAP to find first 15 odd numbers`);
+let k=0;
+while(k<=30){
+if(k %2!=0)
+{
+    console.log(k);
+}
+    k=k+1;
+}
+
+console.log(`-----------------------------------------------------------`);
+
+console.log(`WAP to find first 10 even numbers`);
+let l=0;
+while(l<=10)
+{
+  if(l%2==0)
+  {
+    console.log(l);
+  }
+  l=l+1;
+}
+
+console.log(`---------------------------------------------------------`);
+
+console.log(`WAP to print table of 5`);
+let m=5;
+while(m<=50)
+{
+    if(m % 5 == 0)
+    {
+        console.log(m);
+    }
+    m=m+1;
+}
+console.log(`----------------------------------------------------------------`);
+
+console.log(`WAP to print table of 10`);
+let n=10;
+while(n<=100)
+{
+    if(n % 10 == 0)
+    {
+       console.log(n);
+    }
+    n=n+1;
+}
+
+console.log(`-----------------------------------------------------`);
+
+console.log(`WAP to print table of 10 in reverse order`);
+let r=100;
+while(r>=10){
+    if(r%10==0){
+        console.log(r);
+    }
+    r=r-1;
+}
