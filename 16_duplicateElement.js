f63fa8e01de61a3ed18b98daeab164f662d462ac
Toraskarpg <pgtoraskar@gmail.com>
2023-06-09 12:39:21 +0530

    duplicate Element logic
    duplicate Element logic

---
+++ 16_duplicateElement.js
@@ -0,0 +1,11 @@
+console.log(`-----------------Remove Duplicate element from array------------------`);
+const arrayNum = [11, 3, 4, 11, 4, 7, 3];
+const setOfNum = [...new Set(arrayNum)];
+console.log(setOfNum);
+new Set(arrayNum);
+
+console.log(`----------------Given String Value---------------------------`);
+const str = "How are you mate  ";
+const result = str.split(" ");
+console.log(result);
+
